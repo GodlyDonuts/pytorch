6ac84b0cfd6d45626ce0bf89fe4db42afdc23ca3
Yin Li <215896158+Kevin-Li-2025@users.noreply.github.com>
2026-05-29 20:47:07 +0100

    Fix pytree codegen for tuple dict positional inputs

--- torch/_dynamo/functional_export.py
+++ torch/_dynamo/functional_export.py
@@ -1293,6 +1293,7 @@ def _dynamo_graph_capture_for_export(
                     argument_names(inspect.signature(orig_callable), args, kwargs),  # type: ignore[attr-defined, arg-type]
                     in_spec,
                     out_spec,
+                    True,
                 )
             )
             transformed_graph.recompile()
